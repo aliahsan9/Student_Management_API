2174fbd62cc06007bb6028e92a857d491576c23a
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-17 18:51:32 +0500

    added course repository

---
+++ Repositories/CourseRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Student_Management_API.Data;
+using Student_Management_API.Entities;
+using Student_Management_API.Interfaces;
+
+namespace Student_Management_API.Repositories
+{
+    public class CourseRepository : ICourse   
+    {
+        private readonly AppDbContext _context;
+        public CourseRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Course>> GetAllAsync()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+        public async Task<Course?> GetByIdAsync(int id) 
+        {
+            return await _context.Courses.FindAsync(id);
+        }
+        public async Task AddAsync(Course course)
+        {
+             await _context.AddAsync(course);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Course course)
+        {
+             _context.Courses.Update(course);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Course> DeleteAsync(int id)
+        {
+            var course = await _context.Courses.FindAsync(id);
+            if (course == null)
+                return null!;
+             _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return course;
+        }
+
+    }
+}
