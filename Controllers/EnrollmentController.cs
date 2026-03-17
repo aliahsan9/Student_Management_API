6fca240b2e2d5c996c2a16356d708fe8e0e59483
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-17 20:04:01 +0500

    added enrollment controller

---
+++ Controllers/EnrollmentController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Student_Management_API.Entities;
+using Student_Management_API.Repositories;
+
+namespace Student_Management_API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EnrollmentController : ControllerBase
+    {
+        private readonly EnrollmentRepository _repository;
+        public EnrollmentController(EnrollmentRepository repository)
+        {
+            _repository = repository;
+        }
+        [HttpGet]        
+        public async Task<IActionResult> GetAll()
+        {
+            await _repository.GetAll();
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByIdAsync(int id)
+        {
+            var enrollment = await _repository.GetByIdAsync(id);
+            if (id != enrollment.Id)
+                return NotFound(enrollment);
+            await _repository.GetByIdAsync(id);
+            return Ok();
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddAsync([FromBody] Enrollment enrollment)
+        {
+            await _repository.AddAsync(enrollment);
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAsync(int id, [FromBody] Enrollment enrollment)
+        {
+            if (id != enrollment.Id)
+                return BadRequest();
+            await _repository.UpdateAsync(enrollment);
+            return Ok();
+        }
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+            return Ok();
+        }
+    }
+}
--- Repositories/EnrollmentRepository.cs
+++ Repositories/EnrollmentRepository.cs
@@ -1,6 +1,47 @@
-﻿namespace Student_Management_API.Repositories
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_API.Data;
+using Student_Management_API.Entities;
+using Student_Management_API.Interfaces;
+
+namespace Student_Management_API.Repositories
 {
-    public class EnrollmentRepository
+    public class EnrollmentRepository : IEnrollment
     {
+        private readonly AppDbContext _context;
+        public EnrollmentRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Enrollment>> GetAll()
+        {
+           return await _context.Enrollments.ToListAsync();
+        }
+        public async Task<Enrollment> GetByIdAsync(int id)
+        {
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment == null)
+                return null!;
+            await _context.Enrollments.FindAsync(enrollment);
+            return enrollment;
+        }
+        public async Task AddAsync(Enrollment enrollment)
+        {
+             _context.Enrollments.Add(enrollment);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Enrollment enrollment)
+        {
+             _context.Enrollments.Update(enrollment);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Enrollment> DeleteAsync(int id)
+        {
+            var enrollment = await _context.Enrollments.FindAsync(id);
+            if (enrollment == null)
+                return null!;
+             _context.Enrollments.Remove(enrollment);
+            await _context.SaveChangesAsync();
+            return enrollment;
+        }
     }
 }
