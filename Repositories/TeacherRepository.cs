bd87eaa380d3e718747aacf8aa6a819b96fa828a
Ali Ahsan <aliahsan8751@gmail.com>
2026-03-17 14:15:08 +0500

    added new repository for teacher

---
+++ Repositories/TeacherRepository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Student_Management_API.Data;
+using Student_Management_API.Entities;
+
+namespace Student_Management_API.Repositories
+{
+    public class TeacherRepository
+    {
+        private readonly AppDbContext _context;
+        public TeacherRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+   
+        public async Task<IEnumerable<Teacher>> GetAllAsync()
+        {
+            return await _context.Teachers.ToListAsync();
+        }
+        public async Task<Teacher?> GetByIdAsync(int id)
+        {
+            return await _context.Teachers.FindAsync(id); 
+        }
+        public async Task AddAsync(Teacher teacher)
+        {
+            await _context.Teachers.AddAsync(teacher);
+            await _context.SaveChangesAsync();
+        }
+        public async Task UpdateAsync(Teacher teacher)
+        {
+            _context.Teachers.Update(teacher);
+            await _context.SaveChangesAsync();
+        }
+        public async Task<Teacher> DeleteAsync(int id)
+        {
+            var student = await _context.Teachers.FindAsync(id);
+            if (student == null)
+                return null!;
+
+             _context.Teachers.Remove(student);
+            await _context.SaveChangesAsync();
+            return student;
+        }
+
+      }
+}
